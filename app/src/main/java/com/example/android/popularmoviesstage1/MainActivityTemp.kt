d4cd5bcc361ade30609e63ff1809fde89254d8d2
Deniz Kalem <deka@gmx.de>
2018-08-23 17:57:41 +0200

    Implemented GridView and MenuItems

--- app/src/main/java/com/example/android/popularmoviesstage1/MainActivityTemp.kt
+++ app/src/main/java/com/example/android/popularmoviesstage1/MainActivityTemp.kt
@@ -1,7 +1,15 @@
 package com.example.android.popularmoviesstage1
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import com.example.android.popularmoviesstage1.Constants.Companion.CATEGORY_POPULAR
+import com.example.android.popularmoviesstage1.Constants.Companion.CATEGORY_TOP_RATED
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivityTemp : AppCompatActivity() {
 
@@ -15,5 +23,45 @@ class MainActivityTemp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        gridview_main_movies.adapter = movieAdapter
+        gridview_main_movies.setOnItemClickListener { parent, view, position, id ->
+            var movie: Movie = movieAdapter.getItem(position)
+            var intent = Intent(this, MovieDetailActivity::class.java)
+            intent.putExtra("movie", movie)
+            startActivity(intent)
+        }
+
+        showMovies(CATEGORY_POPULAR)
+    }
+
+    private fun showMovies(filter: String) {
+        textview_main_error_message.visibility = View.INVISIBLE
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onStart() {
+        super.onStart()
+        showMovies(CATEGORY_POPULAR)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        var id: Int = item!!.itemId
+
+        if (id == R.id.action_top_rated) {
+            showMovies(CATEGORY_TOP_RATED)
+            return true;
+        }
+
+        if (id == R.id.action_most_popular || id == R.id.action_refresh) {
+            showMovies(CATEGORY_POPULAR)
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
